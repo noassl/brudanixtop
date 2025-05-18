a08a1ecf428b55f62ce50fe80b479233b3c377df
Janik <jneissl27@gmail.com>
2025-05-18 22:01:15 +0200

    Move home manager stateVersion to home manager module

--- users/home-manager.nix
+++ users/home-manager.nix
@@ -8,7 +8,17 @@
         ../plasma-manager/plasma.nix
         ../user-modules/default.nix
       ];
+
+      # This value determines the Home Manager release that your configuration is
+      # compatible with. This helps avoid breakage when a new Home Manager release
+      # introduces backwards incompatible changes.
+      #
+      # You should not change this value, even if you update Home Manager. If you do
+      # want to update the value, then make sure to first check the Home Manager
+      # release notes.
+      home.stateVersion = "24.11";
     };
+
     sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
   };
 }
