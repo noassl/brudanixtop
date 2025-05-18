68fc584b25921449ecbabf37d595accbed327b07
Janik <jneissl27@gmail.com>
2025-05-18 21:25:49 +0200

    Move home-manager config to users folder

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./locales.nix
-      ../modules/home-manager.nix
+      ../users/home-manager.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
